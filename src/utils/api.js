2e372ba90a7236e0b0c9140d0f1379decdabcea1
EmbergenovAJ <19ayda967198@gmail.com>
2025-05-30 23:39:08 +0500

    url fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,24 @@
 export const fetchUsers = async () => {
-  const response = await fetch("http://localhost:5000/users", {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+  const response = await fetch(
+    "https://backend-user-management-b6b7.onrender.com/users",
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
   return response.json();
 };
 
 export const loginUser = async (email, password) => {
-  const response = await fetch("http://localhost:5000/auth/signin", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
+  const response = await fetch(
+    "https://backend-user-management-b6b7.onrender.com/auth/signin",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    }
+  );
 
   const data = await response.json();
   if (!response.ok) throw new Error(data.message);
@@ -20,33 +26,42 @@ export const loginUser = async (email, password) => {
 };
 
 export const updateUserStatus = async (id, status) => {
-  const response = await fetch(`http://localhost:5000/users/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify({ status }),
-  });
+  const response = await fetch(
+    `https://backend-user-management-b6b7.onrender.com/users/${id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ status }),
+    }
+  );
   return response.json();
 };
 
 export const deleteUser = async (id) => {
-  const response = await fetch(`http://localhost:5000/users/${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+  const response = await fetch(
+    `https://backend-user-management-b6b7.onrender.com/users/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
   return response.json();
 };
 
 export const registerUser = async (name, email, password) => {
-  const response = await fetch("http://localhost:5000/auth/signup", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, email, password }),
-  });
+  const response = await fetch(
+    "https://backend-user-management-b6b7.onrender.com/auth/signup",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password }),
+    }
+  );
 
   const data = await response.json();
   if (!response.ok) throw new Error(data.message);
